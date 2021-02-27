8ee5322406cd182b002c5df9384fa794e4c36bb3
pulkiit <pulkiit@users.noreply.github.com>
2021-02-27 19:52:39 +0400

    70% login complete

--- Warwick 2021/login.js
+++ Warwick 2021/login.js
@@ -1,28 +1,54 @@
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyCAlWxVrlsEraDPWjTjmvfTdOf1MBh5FRI",
     authDomain: "cv-tailor-2021.firebaseapp.com",
     projectId: "cv-tailor-2021",
     storageBucket: "cv-tailor-2021.appspot.com",
     messagingSenderId: "517913762495",
-    appId: "1:517913762495:web:c56701dafe7ed4d6c1040a",
-    measurementId: "G-ZGLEKWHNNX"
+    appId: "1:517913762495:web:9f4ac3a65dd391c8c1040a",
+    measurementId: "G-X4HJHWK20N"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+
 
 const auth =firebase.auth();
 
-function signup(){
+function signUp(){
   var email = document.getElementById("email");
   var password = document.getElementById("password");
 
   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
   promise.catch (e => alert(e.message));
 
-  alert("SignedIn");
+  alert("Signed Up!!");
 
 }
+
+function signIn(){
+
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  promise.catch (e => alert(e.message));
+
+  alert("Welcome " + email.value);
+
+}
+
+function signOut(){
+  
+  auth.signOut();
+  alert("Signed Out !!!");
+}
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var email = user.email;
+    alert("Active User" + email);
+  } else {
+    alert("No Active User");
+  }
+});
