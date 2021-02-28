6c0c5fb03037df9dfbe7382f36027c57af59d9d4
Aaryan Gulia <aaryangulia@Aaryans-MacBook-Pro.local>
2021-02-28 04:42:53 +0300

    FINAL PROJECT

--- cv_create.py
+++ cv_create.py
@@ -16,92 +16,88 @@ def cv_tailor(fd):
     print(f"Phone:       {fd['phone_no']}")
     print(f"LinkedIn:    {fd['linkedin profile']}")
     print(f"Address:     {fd['address']}")
-
     linebreak()
 
-    print(f"Education_")
-    print("______________________________________________________________________________________________________")
+    if fd['education']:
 
-    linebreak()
-    skills = []
+        print(f"Education_")
+        print("______________________________________________________________________________________________________")
 
-    education = fd['education']
+        linebreak()
+        skills = []
 
-    for edu in education:
-        print(f"School:      {edu['organization']}")
-        print(f"Study Area:  {edu['name_of_course']}")
-        print(f"Degree:      {edu['qualification']}")
-        print(f"Graduation:  {edu['date_completed']}")
-        print(f"Grade:       {edu['grade']}")
-        string = "Skills:      "
-        skills = edu['skills']
-        for skill in skills:
-            string += skill + ", "
-        print(string[:-2])
+        education = fd['education']
+
+        for edu in education:
+            print(f"School:      {edu['organization']}")
+            print(f"Study Area:  {edu['name_of_course']}")
+            print(f"Degree:      {edu['qualification']}")
+            print(f"Graduation:  {edu['date_completed']}")
+            print(f"Grade:       {edu['grade']}")
+            string = "Skills:      "
+            skills = edu['skills']
+            for skill in skills:
+                string += skill + ", "
+            print(string[:-2])
+            linebreak()
+
+        print(f"Work_Experience_")
+        print("______________________________________________________________________________________________________")
+
+    if fd['experience']:
         linebreak()
 
-    linebreak()
+        experience = fd['experience']
 
-    print(f"Work_Experience_")
-    print("______________________________________________________________________________________________________")
+        for exp in experience:
+            print(f"Company/Org: {exp['organization']}")
+            print(f"Position:    {exp['name_of_role']}")
+            print(f"Start Year:  {exp['date_start']}")
+            print(f"End Year:    {exp['date_finish']}")
+            print(f"Summary:     {exp['description']}")
+            string = "Skills:      "
+            skills = exp['skills']
+            for skill in skills:
+                string += skill + ", "
 
-    linebreak()
+            print(string[:-2])
+            linebreak()
 
-    experience = fd['experience']
+    if fd['projects']:
+        print(f"Technical_Projects_")
+        print("______________________________________________________________________________________________________")
 
-    for exp in experience:
-        print(f"Company/Org: {exp['organization']}")
-        print(f"Position:    {exp['name_of_role']}")
-        print(f"Start Year:  {exp['date_start']}")
-        print(f"End Year:    {exp['date_finish']}")
-        print(f"Summary:     {exp['description']}")
-        string = "Skills:      "
-        skills = exp['skills']
-        for skill in skills:
-            string += skill + ", "
-
-        print(string[:-2])
         linebreak()
 
-    linebreak()
+        projects = fd['projects']
 
-    print(f"Technical_Projects_")
-    print("______________________________________________________________________________________________________")
+        for project in projects:
+            print(f"Company/Org: {project['name_of_project']}")
+            print(f"Position:    {project['description']}")
+            string = "Skills:      "
+            skills = project['skills']
+            for skill in skills:
+                string += skill + ", "
 
-    linebreak()
+            print(string[:-2])
+            linebreak()
 
-    projects = fd['projects']
-
-    for project in projects:
-        print(f"Company/Org: {project['name_of_project']}")
-        print(f"Position:    {project['description']}")
-        string = "Skills:      "
-        skills = project['skills']
-        for skill in skills:
-            string += skill + ", "
-
-        print(string[:-2])
+    if fd['extra_curriculars']:
+        print(f"Extra-Curriculars_")
+        print("______________________________________________________________________________________________________")
         linebreak()
 
-    linebreak()
+        extra_curriculars = fd['extra_curriculars']
 
-    print(f"Extra-Curriculars_")
-    print("______________________________________________________________________________________________________")
-    linebreak()
+        for ec in extra_curriculars:
+            print(f"Activity:    {ec['name']}")
+            print(f"Company/Org: {ec['organization']}")
 
-    extra_curriculars = fd['extra_curriculars']
+            print(f"Summary:     {ec['description']}")
+            string = "Skills:      "
+            skills = ec['skills']
+            for skill in skills:
+                string += skill + ", "
 
-    for ec in extra_curriculars:
-        print(f"Activity:    {ec['name']}")
-        print(f"Company/Org: {ec['organization']}")
-
-        print(f"Summary:     {ec['description']}")
-        string = "Skills:      "
-        skills = ec['skills']
-        for skill in skills:
-            string += skill + ", "
-
-        print(string[:-2])
-        linebreak()
-
-    linebreak()
+            print(string[:-2])
+            linebreak()
--- main.py
+++ main.py
@@ -11,31 +11,35 @@ if __name__ == '__main__':
 
 db = firestore.client()
 
-print('hi, this is a revolutionary cv building idea which can disrupt existing apps if given sufficient time to develop')
-
 while True:
-    if input('have you filled in the cv building form before?(y/n) ').lower() == 'y':
-        email = input('enter your email: ')
-        get_data = db.collection('persons').where('email','==',email.lower())
-        #print(get_data.get())
+    print('hi, this is a revolutionary cv building idea which can disrupt existing apps if given sufficient time to develop')
 
-        if len(get_data.get()) > 0:
-            form_data = {'email': email}
-            form_data = firebase_get(db,form_data)
-            break
+    while True:
+        if input('have you filled in the cv building form before?(y/n) ').lower() == 'y':
+            email = input('enter your email: ')
+            get_data = db.collection('persons').where('email','==',email.lower())
+
+            if len(get_data.get()) > 0:
+                form_data = {'email': email}
+                form_data = firebase_get(db,form_data)
+                break
+            else:
+                print('the email you entered is not known, please enter n next')
+
         else:
-            print('the email you entered is not known, please enter n next')
+            form_data = get_form_data()
+            firestore_upload(db, form_data)
+            form_data = firebase_get(db, form_data)
+            break
 
-    else:
-        form_data = get_form_data()
-        firestore_upload(db, form_data)
-        form_data = firebase_get(db, form_data)
-        break
+    new_dict = proper_data(db, form_data)
 
-new_dict = proper_data(db, form_data)
+    print('your smart CV for the role is: ')
+    cv_tailor(new_dict)
 
-print('your smart CV for the role is: ')
-cv_tailor(new_dict)
+    print('your normal CV is: ')
+    cv_tailor(form_data)
 
-print('your normal CV is: ')
-cv_tailor(form_data)
+    if input('do you want to re-run the application?(yes/no): ').lower() == yes:
+        continue
+    break
